10f8efc2018a29e960a5f0255b6ba1744b5130f0
sophie-patras <62791972+sophie-patras@users.noreply.github.com>
2025-01-09 17:50:07 +0100

    Dumbsquare/scripts/run/ updtate with yaml versioning

--- pfconfig.sh
+++ pfconfig.sh
@@ -7,31 +7,34 @@
 export EXEC_DIR=/home/patras/PF-Test/DumbSquare/scripts/run/
 cd ${EXEC_DIR}
 # mkdir tmp
-
 export WORK_DIR=/home/patras/PF-Test/DumbSquare/scripts/run/tmp/
 # echo "Using WORK_DIR: ${WORK_DIR}"
 # input files must be in tmp
 
+Geometry=DSc100z10s0
+Physics=REr # es. MX :: Maxwell, 2013 pbm a - quick cv parameters
+Conditions=DP23 # Dupuit y-upper -2, y-lower -3
+Solver=IN0 # NL:Inexact Newton, with Tolerance and else, version 0
 
-run_name=DS.c100s0001
-cellsize=100
+run_name=DSc100z10s0
+#cellsize=100
 
 ## RUN SIMULATION
 echo "Job started at " `date`
-
 # chmod +rx pfsimulation_yaml.py
-python3 pfsimulation_yamlall.py $run_name $cellsize --parflow-directory ${PARFLOW_DIR} --working-directory ${WORK_DIR} --show-line-error --validation-verbose -p 3 -q 3 -r 1
-
+python3 pfsimulation_yamlGPCS.py $run_name --parflow-directory ${PARFLOW_DIR} --working-directory ${WORK_DIR} --show-line-error --validation-verbose -p 2 -q 2 -r 1
 # run_script.py [-h] [--parflow-directory PARFLOW_DIRECTORY] [--parflow-version PARFLOW_VERSION]
 #  [--working-directory WORKING_DIRECTORY] [--skip-validation] [--dry-run] [--show-line-error]
 #  [--exit-on-error] [--write-yaml] [--validation-verbose] [-p P] [-q Q] [-r R]
-
 echo "Job finished at " `date`
 
 rm tmp/${run_name}.*.dist
 ls tmp/
-cp InputKeys_c100s0.yaml tmp/${run_name}.yaml
-#rm /home/patras/Valmalenco/Tmp/${run_name}_new/*
-mkdir /home/patras/PF-Test/DumbSquare/outputs/${run_name}_new/
-mv /home/patras/PF-Test/DumbSquare/scripts/run/tmp/${run_name}.* /home/patras/PF-Test/DumbSquare/outputs/${run_name}_new/ 
-
+cp InputKeysGeometry_${Geometry}.yaml tmp/
+cp InputKeysPhysics_${Physics}.yaml tmp/
+cp InputKeysConditions_${Conditions}.yaml tmp/
+cp InputKeysSolver_${Solver}.yaml tmp/
+#rm -r /home/patras/PF-Test/DumbSquare/outputs/${run_name}_new/
+mkdir /home/patras/PF-Test/DumbSquare/outputs/$Geometry.$Physics.$Conditions.${Solver}_new/
+mv /home/patras/PF-Test/DumbSquare/scripts/run/tmp/${run_name}.* /home/patras/PF-Test/DumbSquare/outputs/$Geometry.$Physics.$Conditions.${Solver}_new/ 
+mv /home/patras/PF-Test/DumbSquare/scripts/run/tmp/InputKeys*.yaml /home/patras/PF-Test/DumbSquare/outputs/$Geometry.$Physics.$Conditions.${Solver}_new/ 
